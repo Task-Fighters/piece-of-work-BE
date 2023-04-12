54cf5de59bc0f9fbf1dd0dfa27504f1f1917bd09
Emanuele <emapage95@gmail.com>
2023-04-12 17:06:14 +0200

    Azure DB setup and added first table

---
+++ salty-server/Models/User.cs
@@ -0,0 +1,24 @@
+namespace salty_server.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    
+    public string GoogleId { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string FullName { get; set; }
+    
+    public string Role { get; set; }
+    
+    public string Location { get; set; }
+    
+    public string Image { get; set; }
+    
+    public string Status { get; set; }
+    
+    public int GroupId { get; set; }
+ 
+    
+}
