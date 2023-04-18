5123d25007dffe54132560a96cb93a244d501c2b
TanLui <effishing@gmail.com>
2023-04-18 14:58:57 +0200

    Refactor Groupbyid endpoint to list username

--- salty-server/Models/GroupResponseDto.cs
+++ salty-server/Models/GroupResponseDto.cs
@@ -5,7 +5,7 @@ public class GroupResponseDto
    
     public int Id { get; set; }
     public string Name { get; set; }
-    public List<int> UsersId {get; set;}
+    public List<UserDetail> UsersId {get; set;}
     public List<int> AssignmentsId {get; set;} 
     
 }
